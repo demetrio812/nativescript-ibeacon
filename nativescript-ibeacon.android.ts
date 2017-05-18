7a5db992698c4dc75e69b0d195fe24c95da08d57
Demetrio Filocamo <filocamo@demetrio.it>
2017-05-19 00:32:37 +0100

    Alpha 2 (initial Android support but it doesn't work yet)

--- nativescript-ibeacon.android.ts
+++ nativescript-ibeacon.android.ts
@@ -1,5 +1,73 @@
-import { Common } from './nativescript-ibeacon.common';
+import {BeaconCallback, BeaconRegion, Common} from './nativescript-ibeacon.common';
+import * as application from "application";
+
+declare var org: any;
+
+export class LocationService {
+
+    private beaconManager: any = null;
+    public delegate: BeaconCallback = null;
+
+
+    private myCallback: any = null;
+
+    constructor() {
+        console.log("constructor");
+    }
+
+    public bind() {
+        this.beaconManager.bind(this.myCallback);
+    }
+
+    public unbind() {
+        this.beaconManager.unbind(this.myCallback);
+    }
+
+    public startRanging(beaconRegion: BeaconRegion) {
+        console.log("startRanging");
+        this.beaconManager = org.altbeacon.beacon.BeaconManager().getInstanceForApplication(application.android.foregroundActivity);
+        this.myCallback = org.altbeacon.beacon.BeaconConsumer.extend({
+
+            onBeaconServiceConnect: function () {
+                console.log("onBeaconServiceConnect");
+
+                // Start ranging
+            }
+
+        });
+
+        this.bind();
+    }
+
+    public stopRanging(beaconRegion: BeaconRegion) {
+        console.log("stopRanging");
+        this.unbind();
+        this.beaconManager = null;
+        this.myCallback = null;
+    }
+
+    private getContext() {
+        let context = application.android.context;
+        return context
+    }
+}
 
 export class NativescriptIbeacon extends Common {
+    private locationService: LocationService = null;
+
+    constructor(beaconCallback: BeaconCallback) {
+        super(beaconCallback);
+        this.locationService = new LocationService();
+        this.locationService.delegate = beaconCallback;
+    }
+
+    public startRanging(beaconRegion: BeaconRegion) {
+        this.locationService.startRanging(beaconRegion);
+    }
+
+    public stopRanging(beaconRegion: BeaconRegion) {
+        this.locationService.stopRanging(beaconRegion);
+    }
+
 
 }
